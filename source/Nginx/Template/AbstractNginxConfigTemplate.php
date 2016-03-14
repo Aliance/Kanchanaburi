136d6bedd1a5bddc568250852461f41b96f5bf4c
Aliance <lesnykh.ilja@gmail.com>
2016-03-14 13:15:02 +0300

    Nginx generator

---
+++ source/Nginx/Template/AbstractNginxConfigTemplate.php
@@ -0,0 +1,39 @@
+<?php
+namespace Aliance\Kanchanaburi\Nginx\Template;
+
+/**
+ * Abstract nginx config template
+ */
+abstract class AbstractNginxConfigTemplate extends AbstractTemplate {
+    /**
+     * @var NginxServer
+     */
+    private $NginxServer;
+
+    /**
+     * @var string listen socket for web requests
+     */
+    public $listen = '';
+
+    /**
+     * @var string[] массив наименований поддерживаемых хостов
+     */
+    public $names = [];
+
+    /**
+     * @var string connection string to FastCGI socket
+     */
+    public $fastcgi = '';
+
+    /**
+     * @param NginxServer $NginxServer экземпляр сервера nginx
+     */
+    public function __construct(NginxServer $NginxServer) {
+        $this->NginxServer = $NginxServer;
+        $this->listen = $this->NginxServer->getListen();
+        $this->names = $this->NginxServer->getServerNames();
+        $this->fastcgi = $this->NginxServer->getSocket();
+        $rolesConfig = Config::getInstance()->get('roles');
+        $this->templateFiles[] = $rolesConfig[$NginxServer->getRole()]['nginx'];
+    }
+}
